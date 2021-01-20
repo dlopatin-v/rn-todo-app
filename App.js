058fcf107fa89c33615ee7b1b1c62d1d892eb6e6
Denis Lopatin <stalk.sdv@gmail.com>
2021-01-21 00:35:19 +0100

    tst

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native'
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Open it</Text>
+      <Text style={styles.text}>Open it ew</Text>
       <StatusBar style='auto' />
     </View>
   )
